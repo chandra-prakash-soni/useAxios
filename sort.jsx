f028ba463057528ac5ccbcc8de88e362c29f4807
Chandra Prakash Soni <cprakash1490@gmail.com>
2024-09-28 19:42:36 +0530

    Update sort.jsx

--- sort.jsx
+++ sort.jsx
@@ -5,7 +5,7 @@ function bubbleSort(arr: number[]): number[] {
     swapped = false;
     for (let i = 0; i < n - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        // Swap
+         
         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
         swapped = true;
       }
