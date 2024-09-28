fe9649d655ff6e61a800c8e0bb0d9fcba34bb10f
Chandra Prakash Soni <cprakash1490@gmail.com>
2024-09-28 19:27:34 +0530

    Create sort.jsx

---
+++ sort.jsx
@@ -0,0 +1,15 @@
+function bubbleSort(arr: number[]): number[] {
+  let n = arr.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
